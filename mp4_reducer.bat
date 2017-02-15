f7edb93bd3995984b4eb91d4209a741d1cb0ea18
zwquery <wawaseb@hotmail.com>
2017-02-15 14:12:43 +0100

    Add files via upload

--- mp4_reducer.bat
+++ mp4_reducer.bat
@@ -3,5 +3,5 @@ REM It needs "ffmpeg" of course
 
 @echo off
 chcp 1252
-for %%a IN (SRC_FOLDER\*.mp4) do D:\sebastienw\Tools\ffmpeg\bin\ffmpeg -i "%%a" -vcodec libx264 -crf 20 "DST_FOLDER\%%~na.mp4"
+for %%a IN (SRC_FOLDER\*.mp4) do FFMPEG_FOLDER\ffmpeg\bin\ffmpeg -i "%%a" -vcodec libx264 -crf 20 "DST_FOLDER\%%~na.mp4"
 pause
