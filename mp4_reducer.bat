1f042c18052d4c84989fa45fde2725b0b82d0e67
zwquery <wawaseb@hotmail.com>
2017-02-15 14:12:02 +0100

    Add files via upload

---
+++ mp4_reducer.bat
@@ -0,0 +1,7 @@
+﻿REM Reduce "mp4" video files
+REM It needs "ffmpeg" of course
+
+@echo off
+chcp 1252
+for %%a IN (SRC_FOLDER\*.mp4) do D:\sebastienw\Tools\ffmpeg\bin\ffmpeg -i "%%a" -vcodec libx264 -crf 20 "DST_FOLDER\%%~na.mp4"
+pause
