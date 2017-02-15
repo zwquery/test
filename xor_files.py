8a4b8a5c86f7e264d9349e951ddeee1e4d203a8b
zwquery <wawaseb@hotmail.com>
2017-02-15 14:00:26 +0100

    Add files via upload

---
+++ xor_files.py
@@ -0,0 +1,43 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# This basic script requires all files are in hardcoded C:\folder\ folder
+#
+# It DOES NOT save files before ! Hopefully, XOR is reversible   :)
+
+import os
+import glob
+
+
+os.system('cls')
+
+
+
+print "Basic Cypher system by ZwQuery"
+print "************************************"
+print ""
+print "Only for Python 2.7"
+print ""
+print ""
+print ""
+print ""
+
+
+name = raw_input("Press a key to encrypt files from here...")
+
+def xor(data, key):
+	l = len(key)
+	return bytearray((
+		(data[i] ^ key[i % l]) for i in range(0,len(data))
+	))
+
+
+for filename in glob.iglob("C:\\folder\\*.*"):
+    with open(filename, "r+b") as f:
+	data = bytearray(open(filename, 'rb').read())
+	key = bytearray([0x36,0x30,0x34])
+	f = open (filename, 'wb')
+	pom = xor(data, key)
+	f.write(pom)
+	print filename + " destroyed !"
+	print ""
+	print "Successfully broken !     :)"
