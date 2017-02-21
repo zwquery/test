12b781e9e04401b2ccee2b0d6ee7dccde7100a6d
zwquery <wawaseb@hotmail.com>
2017-02-21 14:41:13 +0100

    Update mp4_reducer.bat

--- mp4_reducer.bat
+++ mp4_reducer.bat
@@ -1,7 +1,7 @@
-﻿REM Reduce "mp4" video files
+@echo off
+REM Reduce "mp4" video files
 REM It needs "ffmpeg" of course
 
-@echo off
 chcp 1252
 for %%a IN (SRC_FOLDER\*.mp4) do FFMPEG_FOLDER\ffmpeg\bin\ffmpeg -i "%%a" -vcodec libx264 -crf 20 "DST_FOLDER\%%~na.mp4"
 pause
