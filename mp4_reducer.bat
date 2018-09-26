b61fe39a82e04516676c2625d0955824a8fb5fbb
zwquery <wawaseb@hotmail.com>
2018-09-26 16:40:27 +0000

    This is a test

--- mp4_reducer.bat
+++ mp4_reducer.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Reduce "mp4" video files
 REM It needs "ffmpeg" of course
-
 chcp 1252
 for %%a IN (SRC_FOLDER\*.mp4) do FFMPEG_FOLDER\ffmpeg\bin\ffmpeg -i "%%a" -vcodec libx264 -crf 20 "DST_FOLDER\%%~na.mp4"
 pause
+REM Test Git
